51db243c8126ed51569f1e6320e5306730c146e5
Keitatoo <josekeitaro2008@gmail.com>
2026-06-01 17:24:21 +0700

    fix: revert to classic kernel bootstrap for stability

--- api/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Mengarahkan Vercel ke entrypoint Laravel yang asli
-require __DIR__ . '/../public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,26 +1,22 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
-// Mendapatkan root path yang sesungguhnya terlepas dari mana file dipanggil
-$basePath = realpath(__DIR__ . '/../');
+$app = new Application('/var/task/user');
 
-return Application::configure(basePath: $basePath)
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-        $middleware->validateCsrfTokens(except: ['api/*']);
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })->create();
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+return $app;
